707cceb8fd172e7eb831ab39694a9b8c5c93889b
Yeongjin Kim <77034308+7591yj@users.noreply.github.com>
2025-07-06 23:40:10 +0900

    chore: moved menu items to @/lib/app-sidebar-items.ts

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,10 +1,4 @@
-import {
-  Bot,
-  ChartPie,
-  ChevronDown, FileText, Map,
-  Settings2,
-  SquareTerminal
-} from "lucide-react";
+import { ChevronDown } from "lucide-react";
 
 import {
   Sidebar,
@@ -18,43 +12,13 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import {
-  DropdownMenu, DropdownMenuContent, DropdownMenuItem,
-  DropdownMenuTrigger
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-// Menu items.
-const items = [
-  {
-    title: "사업평가",
-    url: "#",
-    icon: SquareTerminal,
-  },
-  {
-    title: "모델 선택",
-    url: "#",
-    icon: Bot,
-  },
-  {
-    title: "분석 설정",
-    url: "#",
-    icon: Settings2,
-  },
-  {
-    title: "보고서",
-    url: "#",
-    icon: FileText,
-  },
-  {
-    title: "시각화",
-    url: "#",
-    icon: ChartPie,
-  },
-  {
-    title: "로드맵",
-    url: "#",
-    icon: Map,
-  },
-];
+import items from "@/lib/app-sidebar-items";
 
 export function AppSidebar() {
   return (
---
+++ src/lib/app-sidebar-items.ts
@@ -0,0 +1,42 @@
+import {
+  Bot,
+  ChartPie,
+  FileText, Map,
+  Settings2,
+  SquareTerminal
+} from "lucide-react";
+
+const items = [
+  {
+    title: "사업평가",
+    url: "#",
+    icon: SquareTerminal,
+  },
+  {
+    title: "모델 선택",
+    url: "#",
+    icon: Bot,
+  },
+  {
+    title: "분석 설정",
+    url: "#",
+    icon: Settings2,
+  },
+  {
+    title: "보고서",
+    url: "#",
+    icon: FileText,
+  },
+  {
+    title: "시각화",
+    url: "#",
+    icon: ChartPie,
+  },
+  {
+    title: "로드맵",
+    url: "#",
+    icon: Map,
+  },
+];
+
+export default items;
