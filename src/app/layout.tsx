9c2d9ce22a82a03cedccb2329833f7ffa5cf2bdc
Yeongjin Kim <77034308+7591yj@users.noreply.github.com>
2025-07-11 23:13:23 +0900

    feat: added basic routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,30 +7,24 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 
 export const metadata: Metadata = {
-  title: "BizLenz",
-  description: "Business proposals, at your finest.",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+    title: "BizLenz",
+    description: "Business proposals, at your finest.",
+    icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 const geist = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
+    subsets: ["latin"],
+    variable: "--font-geist-sans",
 });
 
 export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en" className={`dark ${geist.variable}`}>
-      <body>
-        <SidebarProvider>
-          <AppSidebar />
-          <main>
-            <SidebarTrigger />
-            {children}
-          </main>
-        </SidebarProvider>
-      </body>
-    </html>
-  );
+                                       children,
+                                   }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="en" className={`dark ${geist.variable}`}>
+        <body>
+        {children}
+        </body>
+        </html>
+    );
 }
