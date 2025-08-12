128e4b7cc6b84f8015d3a53e8cfccdb68ef859c0
Yeongjin Kim <77034308+7591yj@users.noreply.github.com>
2025-08-12 18:20:10 +0900

    feat: add routing to table

--- src/components/files-table.tsx
+++ src/components/files-table.tsx
@@ -1,230 +1,238 @@
-import * as React from "react"
+"use client";
+
+import * as React from "react";
 import {
-    type ColumnDef,
-    type ColumnFiltersState,
-    flexRender,
-    getCoreRowModel,
-    getFilteredRowModel,
-    getPaginationRowModel,
-    getSortedRowModel,
-    type SortingState,
-    useReactTable,
-    type VisibilityState,
-} from "@tanstack/react-table"
-import {toast} from "sonner"
-import {Button} from "@/components/ui/button"
-import {Checkbox} from "@/components/ui/checkbox"
-import {Input} from "@/components/ui/input"
-import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow,} from "@/components/ui/table"
+  type ColumnDef,
+  type ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  type SortingState,
+  useReactTable,
+  type VisibilityState,
+} from "@tanstack/react-table";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import StatusBubble from "@/components/common/status-bubble";
-import type {File} from "@/types/file"
-import {AnalyzeButton} from "@/components/analyze/analyze-button";
+import type { File } from "@/types/file";
+import { AnalyzeButton } from "@/components/analyze/analyze-button";
+import { useRouter } from "next/navigation";
 
-export const columns: ColumnDef<File>[] = [
+export function FilesTable({ data }: { data: File[] }) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    [],
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+  const [globalFilter, setGlobalFilter] = React.useState("");
+
+  const router = useRouter();
+
+  const columns: ColumnDef<File>[] = [
     {
-        id: "select",
-        header: ({table}) => (
-            <Checkbox
-                checked={
-                    table.getIsAllRowsSelected() ||
-                    (table.getIsSomeRowsSelected() && "indeterminate")
-                }
-                onCheckedChange={(value) => table.toggleAllRowsSelected(!!value)}
-                aria-label="Select all"
-            />
-        ),
-        cell: ({row}) => (
-            <Checkbox
-                checked={row.getIsSelected()}
-                onCheckedChange={(value) => row.toggleSelected(!!value)}
-                aria-label="Select row"
-            />
-        ),
-        enableSorting: false,
-        enableHiding: false,
-        size: 40,
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllRowsSelected() ||
+            (table.getIsSomeRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
+      size: 40,
     },
     {
-        accessorKey: "fileName",
-        header: "파일 이름",
-        cell: ({row}) => (
-            <div className="font-medium">{row.getValue("fileName")}</div>
-        ),
+      accessorKey: "fileName",
+      header: "파일 이름",
+      cell: ({ row }) => (
+        <div className="font-medium">{row.getValue("fileName")}</div>
+      ),
     },
     {
-        accessorKey: "uploadDate",
-        header: "업로드 날짜",
+      accessorKey: "uploadDate",
+      header: "업로드 날짜",
     },
     {
-        accessorKey: "status",
-        header: "상태",
-        cell: ({row}) => {
-            const status = row.getValue<File["status"]>("status")
-            return <StatusBubble status={status}/>;
-        },
+      accessorKey: "status",
+      header: "상태",
+      cell: ({ row }) => {
+        const status = row.getValue<File["status"]>("status");
+        return <StatusBubble status={status} />;
+      },
     },
     {
-        id: "actions",
-        header: () => null,
-        cell: ({row}) => {
-            const file = row.original
-            const isCompleted = file.status === "완료"
+      id: "actions",
+      header: () => null,
+      cell: ({ row }) => {
+        const file = row.original;
+        const isCompleted = file.status === "완료";
 
-            return (
-                <div className="text-right">
-                    {isCompleted ? (
-                        <Button
-                            variant="secondary"
-                            size="sm"
-                            onClick={() =>
-                                toast.info(
-                                    `'${file.fileName}'의 결과 페이지로 이동합니다.`
-                                )
-                            }
-                        >
-                            결과확인
-                        </Button>
-                    ) : (
-                        <AnalyzeButton
-                            fileId={file.id}
-                            fileName={file.fileName}
-                        />
-                    )}
-                </div>
-            )
-        },
-        size: 120,
+        return (
+          <div className="text-right">
+            {isCompleted ? (
+              <Button
+                variant="secondary"
+                size="sm"
+                onClick={() => {
+                  toast.info(`'${file.fileName}'의 결과 페이지로 이동합니다.`);
+                  router.push(`/results/${file.id}`);
+                }}
+              >
+                결과확인
+              </Button>
+            ) : (
+              <AnalyzeButton fileId={file.id} fileName={file.fileName} />
+            )}
+          </div>
+        );
+      },
+      size: 120,
     },
-]
+  ];
 
-export function FilesTable({data}: { data: File[] }) {
-    const [sorting, setSorting] = React.useState<SortingState>([])
-    const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-        []
-    )
-    const [columnVisibility, setColumnVisibility] =
-        React.useState<VisibilityState>({})
-    const [rowSelection, setRowSelection] = React.useState({})
-    const [globalFilter, setGlobalFilter] = React.useState("")
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    onGlobalFilterChange: setGlobalFilter,
+    initialState: {
+      pagination: {
+        pageSize: 10,
+      },
+    },
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+      globalFilter,
+    },
+  });
 
-    const table = useReactTable({
-        data,
-        columns,
-        onSortingChange: setSorting,
-        onColumnFiltersChange: setColumnFilters,
-        getCoreRowModel: getCoreRowModel(),
-        getPaginationRowModel: getPaginationRowModel(),
-        getSortedRowModel: getSortedRowModel(),
-        getFilteredRowModel: getFilteredRowModel(),
-        onColumnVisibilityChange: setColumnVisibility,
-        onRowSelectionChange: setRowSelection,
-        onGlobalFilterChange: setGlobalFilter,
-        initialState: {
-            pagination: {
-                pageSize: 10,
-            },
-        },
-        state: {
-            sorting,
-            columnFilters,
-            columnVisibility,
-            rowSelection,
-            globalFilter,
-        },
-    })
-
-    return (
-        <div className="w-full space-y-4">
-            <div className="flex items-center">
-                <Input
-                    placeholder="파일 이름으로 검색..."
-                    value={globalFilter ?? ""}
-                    onChange={(event) => setGlobalFilter(event.target.value)}
-                    className="max-w-sm"
-                />
-            </div>
-            <div className="rounded-md border">
-                <Table className="table-fixed">
-                    <TableHeader>
-                        {table.getHeaderGroups().map((headerGroup) => (
-                            <TableRow key={headerGroup.id}>
-                                {headerGroup.headers.map((header) => {
-                                    return (
-                                        <TableHead
-                                            key={header.id}
-                                            style={{
-                                                width:
-                                                    header.getSize() !== 150
-                                                        ? `${header.getSize()}px`
-                                                        : undefined,
-                                            }}
-                                        >
-                                            {header.isPlaceholder
-                                                ? null
-                                                : flexRender(
-                                                    header.column.columnDef.header,
-                                                    header.getContext()
-                                                )}
-                                        </TableHead>
-                                    )
-                                })}
-                            </TableRow>
-                        ))}
-                    </TableHeader>
-                    <TableBody>
-                        {table.getRowModel().rows?.length ? (
-                            table.getRowModel().rows.map((row) => (
-                                <TableRow
-                                    key={row.id}
-                                    data-state={row.getIsSelected() && "selected"}
-                                >
-                                    {row.getVisibleCells().map((cell) => (
-                                        <TableCell key={cell.id}>
-                                            {flexRender(
-                                                cell.column.columnDef.cell,
-                                                cell.getContext()
-                                            )}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            ))
-                        ) : (
-                            <TableRow>
-                                <TableCell
-                                    colSpan={columns.length}
-                                    className="h-24 text-center"
-                                >
-                                    결과가 없습니다.
-                                </TableCell>
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-            </div>
-            <div className="flex items-center justify-between">
-                <div className="flex-1 text-sm text-muted-foreground">
-                    {table.getFilteredSelectedRowModel().rows.length} /{" "}
-                    {table.getFilteredRowModel().rows.length} 행 선택됨.
-                </div>
-                <div className="flex items-center space-x-2">
-                    <Button
-                        variant="ghost"
-                        className="text-sm text-muted-foreground hover:bg-transparent hover:text-foreground"
-                        onClick={() => table.previousPage()}
-                        disabled={!table.getCanPreviousPage()}
+  return (
+    <div className="w-full space-y-4">
+      <div className="flex items-center">
+        <Input
+          placeholder="파일 이름으로 검색..."
+          value={globalFilter ?? ""}
+          onChange={(event) => setGlobalFilter(event.target.value)}
+          className="max-w-sm"
+        />
+      </div>
+      <div className="rounded-md border">
+        <Table className="table-fixed">
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead
+                      key={header.id}
+                      style={{
+                        width:
+                          header.getSize() !== 150
+                            ? `${header.getSize()}px`
+                            : undefined,
+                      }}
                     >
-                        이전
-                    </Button>
-                    <Button
-                        variant="ghost"
-                        className="text-sm text-muted-foreground hover:bg-transparent hover:text-foreground"
-                        onClick={() => table.nextPage()}
-                        disabled={!table.getCanNextPage()}
-                    >
-                        다음
-                    </Button>
-                </div>
-            </div>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  결과가 없습니다.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-between">
+        <div className="text-muted-foreground flex-1 text-sm">
+          {table.getFilteredSelectedRowModel().rows.length} /{" "}
+          {table.getFilteredRowModel().rows.length} 행 선택됨.
         </div>
-    )
+        <div className="flex items-center space-x-2">
+          <Button
+            variant="ghost"
+            className="text-muted-foreground hover:text-foreground text-sm hover:bg-transparent"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            이전
+          </Button>
+          <Button
+            variant="ghost"
+            className="text-muted-foreground hover:text-foreground text-sm hover:bg-transparent"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            다음
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
