1f914cb3acfadf7759753905cd6c66da9546a805
Yeongjin Kim <77034308+7591yj@users.noreply.github.com>
2025-08-19 17:30:57 +0900

    chore: remove discord auth / update env.js

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -34,7 +34,6 @@ declare module "next-auth" {
  */
 export const authConfig = {
     providers: [
-        DiscordProvider,
         CognitoProvider({
             clientId: process.env.AUTH_COGNITO_CLIENT_ID,
             clientSecret: process.env.AUTH_COGNITO_CLIENT_SECRET,
@@ -48,9 +47,7 @@ export const authConfig = {
         /**
          * ...add more providers here.
          *
-         * Most other providers require a bit more work than the Discord provider. For example, the
-         * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-         * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
+         * Refer to the NextAuth.js docs for the provider you want to use. Example:
          *
          * @see https://next-auth.js.org/providers/github
          */
