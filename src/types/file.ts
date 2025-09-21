796814a4880350f859dd164a049a3179908cb1a4
Yeongjin Kim <77034308+7591yj@users.noreply.github.com>
2025-09-21 14:37:58 +0900

    feat(files): renew fileSchema

--- src/types/file.ts
+++ src/types/file.ts
@@ -3,8 +3,12 @@ import z from "zod";
 export const fileSchema = z.object({
   id: z.number(),
   file_name: z.string(),
+  file_path: z.string(),
+  mime_type: z.string().optional(),
+  file_size: z.number().optional(),
   created_at: z.string().optional(),
-  size: z.number().optional(),
+  updated_at: z.string().optional(),
+  latest_job_id: z.number().nullish(),
   status: z.enum(["pending", "processing", "completed", "failed"]).optional(),
 });
 
